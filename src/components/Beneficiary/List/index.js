23893af2224721991e4a1a7f38542f4e5c67d113
Gabriel Escame <gabriel.escame@gmail.com>
2021-10-03 19:42:17 -0300

    feat: adding beneficiary list

---
+++ src/components/Beneficiary/List/index.js
@@ -0,0 +1,27 @@
+import Condition from "../../../helpers/Condition";
+
+const List = ({ beneficiaryList }) => {
+  const hasBeneficiaries = beneficiaryList.length > 0;
+  return (
+    <Condition
+      condition={hasBeneficiaries}
+      elseChildren={
+        <p className="cypoc__no-beneficiary">No beneficiaries included.</p>
+      }
+    >
+      <div className="cypoc__beneficiaries-list">
+        {beneficiaryList &&
+          beneficiaryList.map((beneficiary) => {
+            return (
+              <div className="cypoc__beneficiary">
+                <p>{beneficiary?.name}</p>
+                <p>{beneficiary?.percentage}%</p>
+              </div>
+            );
+          })}
+      </div>
+    </Condition>
+  );
+};
+
+export default List;
