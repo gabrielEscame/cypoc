e8e9792f541a47d678081cc8b14c5c98a0d2b80a
Gabriel Escame <gabriel.escame@gmail.com>
2021-10-03 19:41:22 -0300

    feat: inital structure

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.scss'
+import Beneficiary from "./components/Beneficiary";
+
+function App() {
+  return (
+    <div className="app">
+      <Beneficiary />
+    </div>
+  );
+}
+
+export default App;
