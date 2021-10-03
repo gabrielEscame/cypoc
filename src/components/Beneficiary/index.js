ff4bcb82c14566a4bb8e763269eea321d6d5e36f
Gabriel Escame <gabriel.escame@gmail.com>
2021-10-03 19:41:48 -0300

    feat: adding beneficiary component

---
+++ src/components/Beneficiary/index.js
@@ -0,0 +1,69 @@
+import "./style.scss";
+import { useState } from "react";
+
+import Form from "./Form ";
+import List from "./List";
+
+import Condition from "../../helpers/Condition";
+
+const beneficiaryInitialValue = {
+  name: "",
+  percentage: "",
+};
+
+const Beneficiary = () => {
+  const [currentBeneficiary, setCurrentBeneficiary] = useState(
+    beneficiaryInitialValue
+  );
+  const [beneficiaryList, setBeneficiaryList] = useState([]);
+  const [isFormVisible, setIsFormVisible] = useState(false);
+
+  const handleOnChange = (e) => {
+    const { value } = e.target;
+    const field = e.target.getAttribute("field");
+    setCurrentBeneficiary({ ...currentBeneficiary, [field]: value });
+  };
+
+  const handleToggleModal = () => {
+    setIsFormVisible(!isFormVisible);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setBeneficiaryList([...beneficiaryList, currentBeneficiary]);
+    setCurrentBeneficiary(beneficiaryInitialValue);
+    handleToggleModal();
+  };
+
+  const percentageValue = beneficiaryList.reduce((acc, cur) => {
+    return acc + parseInt(cur.percentage);
+  }, 0);
+
+  const isValidStep = percentageValue === 100;
+
+  const buttonClassNamae = `cypoc__next ${
+    !isValidStep ? "cypoc__next--disabled" : ""
+  }`;
+  return (
+    <div className="cypoc">
+      <h1 className="cypoc__title">New beneficiary</h1>
+      <Form
+        handleOnChange={handleOnChange}
+        handleSubmit={handleSubmit}
+        currentBeneficiary={currentBeneficiary}
+        isVisible={isFormVisible}
+        handleClose={handleToggleModal}
+      />
+      <List beneficiaryList={beneficiaryList} />
+      <Condition condition={!isValidStep}>
+        <button className="cypoc__add" onClick={handleToggleModal}>
+          Add beneficiary
+        </button>
+      </Condition>
+
+      <button className={buttonClassNamae}>Next</button>
+    </div>
+  );
+};
+
+export default Beneficiary;
