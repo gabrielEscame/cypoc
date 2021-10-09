07f743dff140c0a99381cfa18ba1372965367824
Gabriel Escame <gabriel.escame@gmail.com>
2021-10-09 11:26:57 -0300

    test: adding remove beneficiary

--- cypress/integration/beneficiary.spec.js
+++ cypress/integration/beneficiary.spec.js
@@ -1,20 +1,43 @@
+Cypress.Commands.add("addBeneficiary", (name, percentage) => {
+  cy.get(".cypoc__add").click();
+  cy.get(".cypoc__form").should("have.class", "cypoc__form--visible");
+  cy.get(".cypoc__form-input").eq(0).type(name);
+  cy.get(".cypoc__form-input")
+    .eq(1)
+    .type(percentage)
+    .should("have.value", percentage);
+  cy.get(".cypoc__form-button").eq(1).click();
+});
+
 describe("Beneficiary flow", () => {
   it("Adding a new single beneficiary", () => {
     cy.visit("/");
 
     cy.get(".cypoc__next").should("have.class", "cypoc__next--disabled");
 
-    cy.get(".cypoc__add").click();
-    cy.get(".cypoc__form").should("have.class", "cypoc__form--visible");
-    cy.get(".cypoc__form-input").eq(0).type("Gabriel Escame");
-    cy.get(".cypoc__form-input").eq(1).type("100").should("have.value", "100");
-    cy.get(".cypoc__form-button").eq(1).click();
+    cy.addBeneficiary("Gabriel Escame", "100");
 
+    cy.get(".cypoc__no-beneficiary").should("not.exist");
     cy.get(".cypoc__add").should("not.exist");
     cy.get(".cypoc__alert-card").should("not.exist");
-    cy.get(".cypoc__no-beneficiary").should("not.exist");
 
     cy.get(".cypoc__beneficiary").should("exist");
     cy.get(".cypoc__next").should("not.have.class", "cypoc__next--disabled");
+  });
+
+  it("Adding and removing beneficiary", () => {
+    cy.visit("/");
+
+    cy.get(".cypoc__next").should("have.class", "cypoc__next--disabled");
+    cy.addBeneficiary("Gabriel Escame", "100");
+
+    cy.get(".cypoc__beneficiary").click();
+    cy.get(".cypoc__beneficiary").should("not.exist");
+
+    cy.get(".cypoc__no-beneficiary").should("exist");
+    cy.get(".cypoc__add").should("exist");
+    cy.get(".cypoc__alert-card").should("exist");
+
+    cy.get(".cypoc__next").should("have.class", "cypoc__next--disabled");
   });
 });
